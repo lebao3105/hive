03cb5f8a1dfba729acfd55f45a8b81928cb780a3
Dishant B <130259005+dishb@users.noreply.github.com>
2023-05-04 10:45:12 -0700

    ⛔ [del] remove faulty build script

--- utils/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pyinstaller -i=./src/icons/dark.png -n=hive --log-level=WARN --clean --noconfirm --onedir --windowed --add-data "$HOME/Library/Python/3.9/lib/python/site-packages/customtkinter:customtkinter/"  "$HOME/Documents/hive/hive.py"
