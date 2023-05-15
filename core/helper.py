f11092c8a98c1cc11585df38a9f303082933b5fa
dishb <code.dishb@gmail.com>
2023-05-14 19:45:02 -0700

    🪄 [fmt] refactor more funcs in helper.py

--- core/helper.py
+++ core/helper.py
@@ -24,25 +24,28 @@ def is_hidden(entity: str, path: str) -> bool:
     Check if the file/directory at the given path is a hidden system file/directory.
     """
 
-    if entity.startswith("."):
+    if entity.startswith("."): # many system files/dirs start with a "."
         return True
 
-    if path in SYSTEM_FILES:
+    if path in SYSTEM_FILES: # if the file/dir is a known system file/dir
         return True
 
     return False
-def can_rename(path: str) -> bool:
+def can_rename(entity: str, path: str) -> bool:
     """
     Checks if it is possible to rename a file or directory based on some conditions.
     """
 
-    if path in NO_RENAME: # if the file is in our list of no-nos
+    if path in NO_RENAME: # if the file/dir is in our list of no-nos
         return False
 
-    if path.startswith("/Library/"): # if the file is in the Library dir
+    if path.startswith("/Library/"): # if the file/dir is in the Library dir
         return False
 
-    if path.startswith("/System/"): # if the file is in the System dir
+    if path.startswith("/System/"): # if the file/dir is in the System dir
+        return False
+
+    if is_hidden(entity, path): # if the file/dir is a system file/dir
         return False
 
     return True
