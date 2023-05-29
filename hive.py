6028224edb169005ff019a3aea67aea0e59c8c20
dishb <code.dishb@gmail.com>
2023-05-29 09:16:47 -0700

    🚀 [feat] added menubar

--- hive.py
+++ hive.py
@@ -17,7 +17,10 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+from webbrowser import open as open_link
 from json import dump, load
+from sys import exit as sys_exit
+from tkinter import Menu
 
 import customtkinter as ctk
 from PIL import ImageTk, Image
@@ -50,6 +53,21 @@ class HiveApp(ctk.CTk):
         elif ctk.get_appearance_mode().lower() == "dark":
             icon_image = Image.open(DARK_ICON_PATH)
             self.iconphoto(True, ImageTk.PhotoImage(icon_image, master = self))
+
+        # setting up the menubar
+        self.menubar = Menu(master = self.master)
+        self.configure(menu = self.menubar)
+
+        # file menu
+        self.file_menu = Menu(master = self.menubar)
+        self.menubar.add_cascade(label = "File", menu = self.file_menu)
+        self.file_menu.add_command(label = "  Quit           ", command = self.quit_app)
+
+        # credits menu
+        self.main_menu = Menu(master = self.menubar)
+        self.menubar.add_cascade(label = "Credits", menu = self.main_menu)
+        self.main_menu.add_command(label = "  Contribute     ", command = self.open_contribute)
+        self.main_menu.add_command(label = "  Creator        ", command = self.open_creator)
 
         # rows (layout)
         self.grid_rowconfigure(0, weight = 0)
@@ -164,6 +182,28 @@ class HiveApp(ctk.CTk):
 
         # save the current config
         self.after(100, self.save_recent)
+
+    def quit_app(self) -> None:
+        """
+        Quits the application.
+        """
+
+        self.destroy()
+        sys_exit(0)
+
+    def open_contribute(self) -> None:
+        """
+        Opens a link to the official Github repository.
+        """
+
+        open_link("https://github.com/dishb/hive")
+
+    def open_creator(self) -> None:
+        """
+        Opens a link to @dishb on Github.
+        """
+
+        open_link("https://github.com/dishb")
 
     def update_tree(self, *args) -> None: # pylint: disable=unused-argument
         """
