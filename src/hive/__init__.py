0b55022d28af72843afd21aed3a1a81aa91a83e9
lebao3105 <bao12345yocoo@gmail.com>
2023-08-07 23:23:30 +0700

    Initial works with libtextworker

--- src/hive/__init__.py
+++ src/hive/__init__.py
@@ -2,3 +2,14 @@
 #	Copyright (C) 2023 Le Bao Nguyen and contributors.
 #	Licensed under the GNU General Public License version 3.0 or later.
 
+from os.path import expanduser
+from libtextworker import THEMES_DIR
+from libtextworker.general import WalkCreation, TOPLV_DIR
+from libtextworker.versioning import require
+from libtextworker._importer import test_import
+
+test_import("wx")
+THEMES_DIR = expanduser("~/.hive/themes")
+TOPLV_DIR = expanduser("~/.hive")
+WalkCreation(THEMES_DIR)
+require("libtextworker", "0.1.4")
