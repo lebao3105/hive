66f6e60ab4f4b1c722a5c5704e1f4d8af9efcab3
lebao3105 <bao12345yocoo@gmail.com>
2023-08-07 23:25:11 +0700

    Use wxDirList from libtextworker + bind events = perfect:)

--- src/hive/mainwindow.py
+++ src/hive/mainwindow.py
@@ -2,18 +2,14 @@
 #	Copyright (C) 2023 Le Bao Nguyen and contributors.
 #	Licensed under the GNU General Public License version 3.0 or later.
 
-import os
 import wx
 import wx.xrc
 
-from libtextworker._importer import test_import
-test_import("wx")
 from libtextworker.general import CraftItems
 from libtextworker.interface.wx.miscs import XMLBuilder, BindMenuEvents
 
-
-from .consts import DATADIR
-from .dialogs import AboutHive, ShowSysSpecs
+from .consts import CURRDIR, DATADIR
+from .dialogs import AboutHive, ShowShortcuts, ShowSysSpecs
 from .notebook import Notebook
 
 class Window(XMLBuilder):
@@ -28,20 +24,39 @@ class Window(XMLBuilder):
             CraftItems(DATADIR, "window.xrc")
         )
 
+        # get what we need
         self.mainFrame: wx.Frame = self.loadObject("MainWindow", "wxFrame")
         self.toolbar: wx.ToolBar = wx.xrc.XRCCTRL(self.mainFrame, "AppToolbar", "wxToolBar")
-
         self.mainFrame.SetSize((860, 640)) # from textworker
+
+        # make aliases
         self.Show = self.mainFrame.Show
         self.Hide = self.mainFrame.Hide
         self.Close = self.mainFrame.Close
 
+        # construct objects
         mainBoxer: wx.BoxSizer = self.mainFrame.GetSizer()
-        self.notebook = Notebook(self.mainFrame)
-        mainBoxer.Add(self.notebook, 1, wx.GROW, 5)
+        splitBox = wx.SplitterWindow(self.mainFrame)
 
+        self.leftDir = wx.GenericDirCtrl(splitBox,
+                                         dir=CURRDIR,
+                                         style=wx.DIRCTRL_DIR_ONLY|wx.DIRCTRL_EDIT_LABELS
+                                         )
+        self.notebook = Notebook(splitBox)
+
+        splitBox.SplitVertically(self.leftDir, self.notebook, 246)
+        mainBoxer.Add(splitBox, 1, wx.GROW, 5)
+
+        # bind events
         self.BindMenu()
         # self.BindToolbar()
+
+        self.leftDir.GetTreeCtrl().Bind(
+            wx.EVT_TREE_ITEM_ACTIVATED,
+            lambda evt: (self.notebook.GetCurrentPage().GoDir(path=self.leftDir.GetPath()))
+        )
+
+        # layout the frame
         self.mainFrame.Layout()
         self.mainFrame.Centre()
     
@@ -61,7 +76,7 @@ class Window(XMLBuilder):
         ]
 
         helpmenu_events = [
-            # Shortcuts is not available
+            (lambda evt: ShowShortcuts(self.mainFrame), 0),
             (AboutHive(self.mainFrame).ShowBox, 1),
             (lambda evt: ShowSysSpecs(self.mainFrame), 2)
         ]
--- src/hive/notebook.py
+++ src/hive/notebook.py
@@ -5,22 +5,7 @@
 import os
 import wx
 import wx.lib.agw.flatnotebook as FLB
-from libtextworker.interface.wx.dirctrl import DirCtrl
-
-IMGS = wx.ImageList(16, 16)
-
-def addImg(name: str):
-    IMGS.Add(
-        wx.ArtProvider.GetBitmap(
-            getattr(wx, f"ART_{name.upper()}"),
-            wx.ART_OTHER,
-            (16, 16)
-        )
-    )
-
-addImg("folder")
-addImg("normal_file")
-addImg("folder_open")
+from libtextworker.interface.wx.dirctrl import DirList, imgs, folderidx
 
 class Notebook(FLB.FlatNotebook):
     """
@@ -29,12 +14,12 @@ class Notebook(FLB.FlatNotebook):
     """
     
     def __init__(
-            self,
-            parent: wx.Window | None = None,
-            id = wx.ID_ANY,
-            pos: wx.Point = wx.DefaultPosition,
-            size: wx.Point = wx.DefaultSize
-        ):
+        self,
+        parent: wx.Window | None = None,
+        id = wx.ID_ANY,
+        pos: wx.Point = wx.DefaultPosition,
+        size: wx.Point = wx.DefaultSize
+    ):
 
         # Custom styles
         # Their name say everything they do
@@ -42,11 +27,10 @@ class Notebook(FLB.FlatNotebook):
             FLB.FNB_ALLOW_FOREIGN_DND | \
             FLB.FNB_DEFAULT_STYLE | \
             FLB.FNB_HIDE_ON_SINGLE_TAB | \
-            FLB.FNB_NAV_BUTTONS_WHEN_NEEDED | \
-            FLB.FNB_NO_NAV_BUTTONS
+            FLB.FNB_NAV_BUTTONS_WHEN_NEEDED
         
         FLB.FlatNotebook.__init__(self, parent, id, pos, size, agwStyle=styles)
-        self.SetImageList(IMGS)
+        self.AssignImageList(imgs)
         self.Bind(FLB.EVT_FLATNOTEBOOK_PAGE_CLOSED, self.OnPageClosed)
     
     def NewTab(self, evt: wx.Event | None = None, path: str = os.path.expanduser("~/")):
@@ -54,12 +38,10 @@ class Notebook(FLB.FlatNotebook):
         Whatever create a new tab.
         """
         self.Freeze()
-        # TODO: Editable list view (libtextworker)
-        # Now use libtextworker's DirCtrl first
-        new = DirCtrl(self, -1)
-        new.SetFolder(path)
+        new = DirList(self)
+        new.DrawItems(path)
 
-        self.AddPage(new, path, True, 0)
+        self.AddPage(new, path, True, folderidx)
         self.Thaw()
     
     def OnPageClosed(self, evt):
